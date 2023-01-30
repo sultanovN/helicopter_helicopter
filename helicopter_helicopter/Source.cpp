2fe9803b4ea5084a5f05066c8948c6aa6b83ab55
sultanovN <104188736+sultanovN@users.noreply.github.com>
2023-01-30 23:07:39 +0600

    01

--- helicopter_helicopter/Source.cpp
+++ helicopter_helicopter/Source.cpp
@@ -1,7 +1,13 @@
 #include "raylib.h"
 #include <stdint.h>
+#include <string>
+#include <vector>
+#include <chrono>
+#include <iostream>
 
 bool game_over = false;
+
+uint8_t level = 0;
 
 class Projectile;
 
@@ -56,7 +62,7 @@ public:
 			m_x += m_speed * GetFrameTime();
 		}
 	}
-
+	
 	void stop() { m_speed = 0; }
 
 	bool collide(Projectile projectile);
@@ -66,14 +72,19 @@ public:
 
 class Projectile
 {
-private:
+protected:
 	float m_x, m_y;
 	float m_width, m_height;
 	float m_speed, m_delay;
 	Color m_color;
 public:
-	Projectile(float delay = 1.0f, Color color = RED, float speed = 300, float width = 80, float height = 40)
-		: m_delay{ delay }, m_x{ GetScreenWidth() + width }, m_y{ GetScreenHeight() / 2.0f - height / 2 }, m_width{ width }, m_height{ height }, m_speed{ speed }, m_color{ color }
+	Projectile(float h_y = 0, float delay = 1.0f, Color color = RED, float speed = 300, float width = 80, float height = 40)
+		: m_delay{ delay }, m_x{ GetScreenWidth() + width }, m_y{ GetScreenHeight() / 2.0f - height / 2 + h_y}, m_width{ width }, m_height{ height }, m_speed{ speed }, m_color{ color }
+	{
+
+	}
+
+	~Projectile()
 	{
 
 	}
@@ -91,38 +102,26 @@ public:
 		DrawRectangleRec({ m_x, m_y, m_width, m_height }, m_color);
 	}
 
-	void move()
+	virtual void move() 
 	{
 		m_x -= m_speed * GetFrameTime();
-	}
-
-	void randomMove()
-	{
-		move();
-		if (-m_x *  - m_delay + m_width < 0)
+		if (-m_x * -m_delay + m_width < 0)
 		{
-			m_speed *= 1.05f;
+			
 			m_x = (GetScreenWidth() + m_width);
 			if (m_y > GetScreenHeight() - GetScreenHeight() * 0.05)
 				m_y = m_height + GetScreenHeight() * 0.05;
 			else
 				m_y += GetScreenHeight() * 0.10;
-		}
-		
-	}
-	/*m_x + m_width < -GetScreenWidth()*/
-	void track(Player player)
-	{
-		move();
-		if ( m_x  - m_width * -m_delay < 0)
-		{
-			m_speed *= 1.05f;
-			m_x = (GetScreenWidth() + m_width);
-			m_y = player.getY();
+			if(m_speed < 600)
+				m_speed *= 1.03f;
 		}
 	}
 
-	void gameEnd(Player &player, Projectile array[])
+	/*m_x + m_width < -GetScreenWidth()*/
+	
+
+	void gameEnd(Player &player, std::vector<Projectile> &array, std::chrono::high_resolution_clock::time_point &time)
 	{
 		if (game_over)
 		{
@@ -133,11 +132,40 @@ public:
 			{
 				game_over = false;
 				player = Player{};
-				array[0] = {};
-				array[1] = { 1.0, VIOLET, 350 };
-				array[2] = { 2.5, PURPLE, 350 };
-				array[3] = { 3.0, DARKBLUE, 450 };
+				array = { {} };
+				time = std::chrono::high_resolution_clock::now();
 			}
+			
+		}
+	}
+};
+
+class TrackingProjectile : public Projectile
+{
+	float m_x, m_y;
+	float m_width, m_height;
+	float m_speed, m_delay;
+	Color m_color;
+public:
+	TrackingProjectile()
+		: Projectile{}
+	{
+
+	}
+
+	~TrackingProjectile()
+	{
+
+	}
+
+	void track(Player player)
+	{
+		m_x -= m_speed * GetFrameTime();
+		if ( m_x  - m_width * -m_delay < 0)
+		{
+			m_speed *= 1.05f;
+			m_x = (GetScreenWidth() + m_width);
+			m_y = player.getY();
 		}
 	}
 };
@@ -151,34 +179,75 @@ bool Player::collide(Projectile projectile)
 	return game_over;
 }
 
+void gameProgress(std::vector<Projectile> &array, std::vector<TrackingProjectile> &track_array , std::chrono::high_resolution_clock::time_point time)
+{
+	std::chrono::high_resolution_clock::time_point time_p = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double> time_span = std::chrono::duration_cast<std::chrono::seconds>(time_p - time);
+	if (time_span.count() > 8 && level == 0)
+	{
+		++level;
+		array.push_back({});
+	}
+	if (time_span.count() > 18 && level == 1)
+	{
+		++level;
+		track_array.push_back(TrackingProjectile{});
+	}
+	if (time_span.count() > 35 && level == 2)
+	{
+		++level;
+		array.push_back({});
+	}
+}
 
+//{60, 1.5}, {320, 1.8}, {-270, 2.5}
+void showTime(std::chrono::high_resolution_clock::time_point& time)
+{
+	std::chrono::high_resolution_clock::time_point time_p;
+	std::chrono::duration<int> time_span;
+	if (!game_over)
+	{
+		time_p = std::chrono::high_resolution_clock::now();
+		time_span = std::chrono::duration_cast<std::chrono::seconds>(time_p - time);
+		
+		
+	}
+	std::string s_time = std::to_string(time_span.count());
+	DrawText(s_time.c_str(), GetScreenWidth() / 2 - MeasureText(s_time.c_str(), 20) / 2, 40, 20, WHITE);
 
+}
 
 int main()
 {
-	#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
+	
 	InitWindow(1000, 800, "Helicopter helicopter");
 
 	SetWindowState(FLAG_VSYNC_HINT);
 
 	Player player;
-	const uint8_t enemy = 4;
-	Projectile array[enemy]{ {}, {1.0, VIOLET, 350}, {2.5, PURPLE, 350}, {3.0, DARKBLUE, 450 } };
+	std::vector<Projectile> array{ {} };
+	std::vector<TrackingProjectile> track_array{};
 	/*Projectile projectileCommon;
 	Projectile projectileTracker{1.0, VIOLET};
 	Projectile projectileTracker1{1.7, PURPLE};*/
 
+	std::chrono::high_resolution_clock::time_point time_1 = std::chrono::high_resolution_clock::now();
 
 	while (!WindowShouldClose())
 	{
-
+		
 		player.move();
-		array[0].randomMove();
-		for (int i = 1; i < enemy; ++i)
-			array[i].track(player);
+		for (auto &elem: array)
+			elem.move();
+		for (auto& elem : track_array)
+			elem.track(player);
+
+		gameProgress(array,track_array, time_1);
+
+
 		BeginDrawing();
 		ClearBackground(BLACK);
-
+		showTime(time_1);
 		player.draw();
 		for (auto elem : array)
 		{
@@ -187,7 +256,7 @@ int main()
 				break;
 
 		}
-		array[0].gameEnd(player, array);
+		array[0].gameEnd(player, array, time_1);
 
 		EndDrawing();
 	}
